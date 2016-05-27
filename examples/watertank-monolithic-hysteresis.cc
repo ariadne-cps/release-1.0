580e48094f29b38276caee896550612493050e9f
Luca Geretti <luca.geretti@gmail.com>
2016-05-27 14:57:51 +0200

    Added evolution analysis for watertank

--- examples/watertank-monolithic-hysteresis.cc
+++ examples/watertank-monolithic-hysteresis.cc
@@ -36,6 +36,25 @@ int main(int argc,char *argv[])
 	// The system
 	HybridAutomaton system = Ariadne::getWatertankMonolithicHysteresis();
 
+    HybridEvolver evolver(system);
+    evolver.verbosity = verb;
+
+    HybridSpace hspace(system.state_space());
+    for (HybridSpace::const_iterator hs_it = hspace.begin(); hs_it != hspace.end(); ++hs_it) {
+        evolver.settings().minimum_discretised_enclosure_widths[hs_it->first] = Vector<Float>(2,3.0);
+        evolver.settings().hybrid_maximum_step_size[hs_it->first] = 0.5;
+    }
+
+    HybridEvolver::EnclosureType initial_enclosure(DiscreteLocation("opened"),Box(2, 6.5,6.5, 1.0,1.0));
+
+    HybridTime evol_limits(80.0,5);
+
+    HybridEvolver::OrbitType orbit = evolver.orbit(initial_enclosure,evol_limits,UPPER_SEMANTICS);
+
+    PlotHelper plotter(system.name());
+    plotter.plot(orbit.reach(),"reach");
+
+	/*
 	// The initial values
 	HybridBoundedConstraintSet initial_set(system.state_space());
 	initial_set[DiscreteLocation("opened")] = Box(2, 6.0,7.5, 1.0,1.0);
@@ -45,7 +64,7 @@ int main(int argc,char *argv[])
 	HybridBoxes domain(system.state_space(),Box(2,4.5,9.0,0.0,1.0));
 
     int accuracy = 4;
-    HybridTime evol_limits(60.0,16);
+    HybridTime evol_limits(160.0,16);
 
     HybridReachabilityAnalyser analyser(system,domain,accuracy);
     analyser.verbosity = verb;
@@ -58,7 +77,7 @@ int main(int argc,char *argv[])
         PlotHelper plotter(system.name());
         plotter.plot(upper_reach,"upper",accuracy);
     }
-
+	*/
 	/*
 
 	// The safety constraint
