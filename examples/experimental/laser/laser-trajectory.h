3db6940268cb1012b342882f21aaa9a444f31c6b
Luca Geretti <luca.geretti@gmail.com>
2016-07-12 14:21:23 +0200

    Simplified the trajectory of the laser

--- examples/experimental/laser/laser-trajectory.h
+++ examples/experimental/laser/laser-trajectory.h
@@ -22,91 +22,48 @@ HybridIOAutomaton getLaserTrajectory()
     HybridIOAutomaton automaton("trajectory");
 
     // Parameters
-    RealParameter accel("accel",300.0); // Acceleration (modulus) of the laser
-    RealParameter half_width("half_width",0.023); // Half width of the cut (excluding the accel/decel tails)
+    RealParameter velocity("velocity",0.46); // Velocity of the laser
+    RealParameter half_width("half_width",0.023); // Half width of the cut
 
     /// Modes
 
-    DiscreteLocation accelerating_right("accelerating_right");
-    DiscreteLocation decelerating_right("decelerating_right");
-    DiscreteLocation accelerating_left("accelerating_left");
-    DiscreteLocation decelerating_left("decelerating_left");
     DiscreteLocation passing_right("passing_right");
     DiscreteLocation passing_left("passing_left");
 
-	automaton.new_mode(accelerating_right);
-	automaton.new_mode(decelerating_right);
-	automaton.new_mode(accelerating_left);
-	automaton.new_mode(decelerating_left);
 	automaton.new_mode(passing_right);
 	automaton.new_mode(passing_left);
 
     // Variables
 
     RealVariable x("x"); // Position of the laser
-    RealVariable vx("vx"); // Speed of the laser
 
-    automaton.add_internal_var(vx);
     automaton.add_output_var(x);
 
     // Events
 
-    DiscreteEvent accelerate_right("accelerate_right");
-    DiscreteEvent stop_accelerating_right("stop_accelerating_right");
-    DiscreteEvent decelerate_right("decelerate_right");
-    DiscreteEvent accelerate_left("accelerate_left");
-    DiscreteEvent stop_accelerating_left("stop_accelerating_left");
-    DiscreteEvent decelerate_left("decelerate_left");
+    DiscreteEvent switch_left("switch_right");
+    DiscreteEvent switch_right("switch_left");
 
 	// Dynamics
 
-	RealExpression dyn_vx_accel = accel;
-	RealExpression dyn_vx_decel = -accel;
-	RealExpression dyn_vx_passing = 0.0;
+	RealExpression dyn_x_right = velocity;
+	RealExpression dyn_x_left = -velocity;
 
-	automaton.set_dynamics(accelerating_right, vx, dyn_vx_accel);
-	automaton.set_dynamics(decelerating_right, vx, dyn_vx_decel);
-	automaton.set_dynamics(accelerating_left, vx, dyn_vx_decel);
-	automaton.set_dynamics(decelerating_left, vx, dyn_vx_accel);
-	automaton.set_dynamics(passing_right, vx, dyn_vx_passing);
-	automaton.set_dynamics(passing_left, vx, dyn_vx_passing);
-
-	RealExpression dyn_x = vx;
-	automaton.set_dynamics(accelerating_right, x, dyn_x);
-	automaton.set_dynamics(decelerating_right, x, dyn_x);
-	automaton.set_dynamics(accelerating_left, x, dyn_x);
-	automaton.set_dynamics(decelerating_left, x, dyn_x);
-	automaton.set_dynamics(passing_right, x, dyn_x);
-	automaton.set_dynamics(passing_left, x, dyn_x);
+	automaton.set_dynamics(passing_right, x, dyn_x_right);
+	automaton.set_dynamics(passing_left, x, dyn_x_left);
 
 	// Transitions
 
 	RealExpression x_greater_half_width = x - half_width; // x >= half_width
-	RealExpression x_lesser_half_width = -x + half_width; // x <= half_width
 	RealExpression x_lesser_minus_half_width = -x - half_width; // x <= -half_width
-	RealExpression x_greater_minus_half_width = x + half_width; // x >= -half_width
-	RealExpression vx_lesser_zero = -vx; // vx <= 0;
-	RealExpression vx_greater_zero = vx; // vx >= 0;
 
-	std::map<RealVariable,RealExpression> reset_left;
-	reset_left[x] = x;
-	reset_left[vx] = 0.0;
-	std::map<RealVariable,RealExpression> reset_right;
-	reset_right[x] = x;
-	reset_right[vx] = 0.0;
 	std::map<RealVariable,RealExpression> reset_minus_half;
 	reset_minus_half[x] = -half_width;
-	reset_minus_half[vx] = vx;
 	std::map<RealVariable,RealExpression> reset_half;
 	reset_half[x] = half_width;
-	reset_half[vx] = vx;
 
-	automaton.new_forced_transition(accelerate_right,decelerating_left,accelerating_right,reset_left,vx_greater_zero);
-	automaton.new_forced_transition(accelerate_left,decelerating_right,accelerating_left,reset_right,vx_lesser_zero);
-	automaton.new_forced_transition(stop_accelerating_right,accelerating_right,passing_right,reset_minus_half,x_greater_minus_half_width);
-	automaton.new_forced_transition(stop_accelerating_left,accelerating_left,passing_left,reset_half,x_lesser_half_width);
-	automaton.new_forced_transition(decelerate_right,passing_right,decelerating_right,reset_half,x_greater_half_width);
-	automaton.new_forced_transition(decelerate_left,passing_left,decelerating_left,reset_minus_half,x_lesser_minus_half_width);
+	automaton.new_forced_transition(switch_left,passing_right,passing_left,reset_half,x_greater_half_width);
+	automaton.new_forced_transition(switch_right,passing_left,passing_right,reset_minus_half,x_lesser_minus_half_width);
 
 	return automaton;
 }
--- examples/experimental/laser/laser.cc
+++ examples/experimental/laser/laser.cc
@@ -40,7 +40,9 @@ int main(int argc, char* argv[])
     Real x_i = -laser_trajectory.parameter_value("half_width");
     Real T0 = skin_temperature.parameter_value("T0");
     Real pass_period = 0.200;
-    Real vx_i = -4.0*x_i/pass_period;
+    Real vx = -4.0*x_i/pass_period;
+
+    system.substitute(RealParameter("velocity",vx));
 
     /// Create a HybridEvolver object
     HybridEvolver evolver(system);
@@ -48,11 +50,11 @@ int main(int argc, char* argv[])
 
     HybridSpace hspace(system.state_space());
     for (HybridSpace::const_iterator hs_it = hspace.begin(); hs_it != hspace.end(); ++hs_it) {
-        evolver.settings().minimum_discretised_enclosure_widths[hs_it->first] = Vector<Float>(7,2.0);
+        evolver.settings().minimum_discretised_enclosure_widths[hs_it->first] = Vector<Float>(6,2.0);
         evolver.settings().hybrid_maximum_step_size[hs_it->first] = 0.00001;
     }
 
-    Box initial_box(7, /*T*/ T0.lower(),T0.upper(), /*p*/ 0.0,0.0, /*t*/ 0.0,0.0, /*vx*/ vx_i.lower(),vx_i.upper(), /*x*/ x_i.lower(),x_i.upper(), /*z*/ 0.0,0.0, /*zi*/ 0.0,0.0);
+    Box initial_box(6, /*T*/ T0.lower(),T0.upper(), /*p*/ 0.0,0.0, /*t*/ 0.0,0.0, /*x*/ x_i.lower(),x_i.upper(), /*z*/ 0.0,0.0, /*zi*/ 0.0,0.0);
     HybridEvolver::EnclosureType initial_enclosure(DiscreteLocation("work,passing_right,far_from_left,varying,idle"),initial_box);
 
     int num_half_cycles = 1;
