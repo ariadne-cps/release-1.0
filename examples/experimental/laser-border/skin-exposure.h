247691ae05127180f878713530b873b40c4a1d32
Luca Geretti <luca.geretti@gmail.com>
2016-07-21 18:13:10 +0200

    Organized the laser example with common/centre/border directories

---
+++ examples/experimental/laser-border/skin-exposure.h
@@ -0,0 +1,115 @@
+/*****************************************************************************************************
+ *            skin-exposure.h
+ *
+ *  Copyright  2016  Luca Geretti
+ *
+ * Provides the behavior of a measure of power over the skin point when the laser is close.
+ *
+ *****************************************************************************************************/
+
+#include "ariadne.h"
+
+#ifndef SKIN_EXPOSURE_H_
+#define SKIN_EXPOSURE_H_
+
+namespace Ariadne {
+
+HybridIOAutomaton getSkinExposure()
+{
+    /// Parameters
+	RealParameter velocity("velocity",0.46);
+	RealParameter L("L",0.00025);
+	RealParameter x0("x0",0.00);
+
+    /// Build the Hybrid System
+
+    /// Create a HybridAutomaton object
+    HybridIOAutomaton automaton("skin-exposure");
+
+    /// Create the discrete states
+    DiscreteLocation close_from_right_in("close_from_right_in");
+    DiscreteLocation close_from_right_out("close_from_right_out");
+    DiscreteLocation close_from_left_in("close_from_left_in");
+    DiscreteLocation close_from_left_out("close_from_left_out");
+    DiscreteLocation far_from_right_in("far_from_right_in");
+    DiscreteLocation far_from_right_out("far_from_right_out");
+    DiscreteLocation far_from_left_in("far_from_left_in");
+    DiscreteLocation far_from_left_out("far_from_left_out");
+
+    RealVariable x("x");
+    RealVariable p("p");
+
+    automaton.add_input_var(x);
+    automaton.add_output_var(p);
+
+    // Events
+    DiscreteEvent laser_comes_from_right("laser_comes_from_right");
+    DiscreteEvent laser_comes_from_left("laser_comes_from_left");
+    DiscreteEvent laser_leaves_from_right("laser_leaves_from_right");
+    DiscreteEvent laser_leaves_from_left("laser_leaves_from_left");
+    DiscreteEvent laser_crosses_from_left("laser_crosses_from_left");
+    DiscreteEvent laser_crosses_from_right("laser_crosses_from_right");
+    DiscreteEvent switch_left("switch_right");
+    DiscreteEvent switch_right("switch_left");
+
+    automaton.add_internal_event(laser_comes_from_left);
+    automaton.add_internal_event(laser_crosses_from_left);
+    automaton.add_internal_event(laser_leaves_from_left);
+    automaton.add_internal_event(laser_comes_from_right);
+    automaton.add_internal_event(laser_crosses_from_right);
+    automaton.add_internal_event(laser_leaves_from_right);
+    automaton.add_input_event(switch_left);
+    automaton.add_input_event(switch_right);
+
+	automaton.new_mode(close_from_right_in);
+	automaton.new_mode(close_from_right_out);
+	automaton.new_mode(far_from_right_in);
+	automaton.new_mode(far_from_right_out);
+	automaton.new_mode(close_from_left_in);
+	automaton.new_mode(close_from_left_out);
+	automaton.new_mode(far_from_left_in);
+	automaton.new_mode(far_from_left_out);
+
+	RealExpression distance = Ariadne::sqr(x-x0);
+
+	RealExpression dyn_close_from_right = velocity*Ariadne::pi<Real>()/L/L * (x-x0) * Ariadne::sin(Ariadne::pi<Real>()/L/L * distance);
+	RealExpression dyn_close_from_left = -dyn_close_from_right;
+	RealExpression dyn_far = 0.0;
+
+	automaton.set_dynamics(close_from_right_in, p, dyn_close_from_right);
+	automaton.set_dynamics(close_from_right_out, p, dyn_close_from_right);
+	automaton.set_dynamics(far_from_right_in, p, dyn_far);
+	automaton.set_dynamics(far_from_right_out, p, dyn_far);
+	automaton.set_dynamics(close_from_left_in, p, dyn_close_from_left);
+	automaton.set_dynamics(close_from_left_out, p, dyn_close_from_left);
+	automaton.set_dynamics(far_from_left_in, p, dyn_far);
+	automaton.set_dynamics(far_from_left_out, p, dyn_far);
+
+	/// Transitions
+	// Guards
+	RealExpression distance_greater_L = distance - L*L; // distance >= L
+	RealExpression distance_lesser_L = L*L - distance; // distance <= L
+	RealExpression x_greater_x0 = x-x0; // x >= x0
+	RealExpression x_lesser_x0 = x0-x; // x <= x0
+
+	// Resets
+	std::map<RealVariable,RealExpression> reset_zero;
+	reset_zero[p] = 0.0;
+	std::map<RealVariable,RealExpression> reset_one;
+	reset_one[p] = 1.0;
+
+	automaton.new_forced_transition(laser_comes_from_right,far_from_right_in,close_from_right_in,distance_lesser_L);
+	automaton.new_forced_transition(laser_comes_from_left,far_from_left_in,close_from_left_in,distance_lesser_L);
+	automaton.new_forced_transition(laser_crosses_from_right,close_from_right_in,close_from_right_out,reset_one,x_lesser_x0);
+	automaton.new_forced_transition(laser_crosses_from_left,close_from_left_in,close_from_left_out,reset_one,x_greater_x0);
+	automaton.new_forced_transition(laser_leaves_from_right,close_from_right_out,far_from_right_out,reset_zero,distance_greater_L);
+	automaton.new_forced_transition(laser_leaves_from_left,close_from_left_out,far_from_left_out,reset_zero,distance_greater_L);
+	automaton.new_unforced_transition(switch_left,far_from_left_out,far_from_right_in);
+	automaton.new_unforced_transition(switch_right,far_from_right_out,far_from_left_in);
+
+	return automaton;
+}
+
+}
+
+#endif
---
+++ examples/experimental/laser-border/skin-temperature.h
@@ -0,0 +1,79 @@
+/*****************************************************************************************************
+ *            skin_temperature.h
+ *
+ *  Copyright  2016  Luca Geretti
+ *
+ * Provides the behavior of the skin temperature under laser cutting.
+ *
+ *****************************************************************************************************/
+
+#include "ariadne.h"
+
+#ifndef SKIN_TEMPERATURE_H_
+#define SKIN_TEMPERATURE_H_
+
+namespace Ariadne {
+
+HybridIOAutomaton getSkinTemperature()
+{
+    /// Parameters
+	RealParameter lambda("lambda",3.3456e3);
+	RealParameter mu("mu",2.9595e5);
+	RealParameter T0("T0",37.0);
+	RealParameter Tevap("Tevap",100.0);
+
+    /// Build the Hybrid System
+
+    /// Create a HybridAutomaton object
+    HybridIOAutomaton automaton("skin-T");
+
+    /// Create the discrete states
+    DiscreteLocation varying("varying");
+    DiscreteLocation post_evaporating("post_evaporating");
+    DiscreteLocation evaporating("evaporating");
+
+    RealVariable p("p");
+    RealVariable T("T");
+
+    automaton.add_input_var(p);
+    automaton.add_output_var(T);
+
+    // Events
+    DiscreteEvent end_post_evaporating("end_post_evaporating");
+    DiscreteEvent start_evaporating("start_evaporating");
+    DiscreteEvent stop_evaporating("stop_evaporating");
+
+    automaton.add_output_event(start_evaporating);
+    automaton.add_input_event(stop_evaporating);
+    automaton.add_internal_event(end_post_evaporating);
+
+    automaton.new_mode(varying);
+    automaton.new_mode(post_evaporating);
+	automaton.new_mode(evaporating);
+
+	RealExpression dyn_varying = mu*p - lambda*(T-T0);
+	RealExpression dyn_evaporating = 0.0;
+
+	automaton.set_dynamics(varying, T, dyn_varying);
+	automaton.set_dynamics(evaporating, T, dyn_evaporating);
+	automaton.set_dynamics(post_evaporating, T, dyn_varying);
+
+	/// Transitions
+	// Guards
+	RealExpression T_greater_Tevap = T - Tevap; // T >= Tevap
+	RealExpression T_lesser_Tevap_minus_1 = Tevap - 1.0 - T; // T <= Tevap-1
+
+	// Resets
+	std::map<RealVariable,RealExpression> reset_evap;
+	reset_evap[T] = Tevap;
+
+	automaton.new_forced_transition(end_post_evaporating,post_evaporating,varying,T_lesser_Tevap_minus_1);
+	automaton.new_forced_transition(start_evaporating,varying,evaporating,reset_evap,T_greater_Tevap);
+	automaton.new_unforced_transition(stop_evaporating,evaporating,post_evaporating);
+
+	return automaton;
+}
+
+}
+
+#endif
