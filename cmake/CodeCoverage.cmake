074112a6757b6136ba5141ef27da69df5a1f4c83
Luca Geretti <luca.geretti@gmail.com>
2018-09-06 20:55:06 +0200

    Tries using kernel-directory instead of directory

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -159,7 +159,7 @@ function(SETUP_TARGET_FOR_COVERAGE_LCOV)
         COMMAND ${Coverage_EXECUTABLE}
 
         # Capturing lcov counters and generating report
-        COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} --directory . --capture --output-file ${Coverage_NAME}.info
+        COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} --kernel-directory . --capture --output-file ${Coverage_NAME}.info
         # add baseline counters
         COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} -a ${Coverage_NAME}.base -a ${Coverage_NAME}.info --output-file ${Coverage_NAME}.total
         COMMAND ${LCOV_PATH} --gcov-tool ${GCOV_PATH} --remove ${Coverage_NAME}.total ${COVERAGE_LCOV_EXCLUDES} --output-file ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
