c96ede5bf66a7954c7271760db7de4f0683e7c42
Luca Geretti <luca.geretti@gmail.com>
2018-09-07 12:38:53 +0200

    Added branch coverage

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -159,11 +159,11 @@ function(SETUP_TARGET_FOR_COVERAGE_LCOV)
         COMMAND ${Coverage_EXECUTABLE}
 
         # Capturing lcov counters and generating report
-        COMMAND ${LCOV_PATH} --compat-libtool --directory . --capture --output-file ${Coverage_NAME}.info
+        COMMAND ${LCOV_PATH} --compat-libtool --rc lcov_branch_coverage=1 --directory . --capture --output-file ${Coverage_NAME}.info
         # add baseline counters
-        COMMAND ${LCOV_PATH} --compat-libtool -a ${Coverage_NAME}.base -a ${Coverage_NAME}.info --output-file ${Coverage_NAME}.total
-        COMMAND ${LCOV_PATH} --compat-libtool --remove ${Coverage_NAME}.total ${COVERAGE_LCOV_EXCLUDES} --output-file ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
-        COMMAND ${GENHTML_PATH} -o ${Coverage_NAME} ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
+        COMMAND ${LCOV_PATH} --compat-libtool -a ${Coverage_NAME}.base -a ${Coverage_NAME}.info --rc lcov_branch_coverage=1 --output-file ${Coverage_NAME}.total
+        COMMAND ${LCOV_PATH} --compat-libtool --rc lcov_branch_coverage=1 --remove ${Coverage_NAME}.total ${COVERAGE_LCOV_EXCLUDES} --output-file ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
+        COMMAND ${GENHTML_PATH} -o ${Coverage_NAME} --rc lcov_branch_coverage=1 ${PROJECT_BINARY_DIR}/${Coverage_NAME}.info.cleaned
         COMMAND ${CMAKE_COMMAND} -E remove ${Coverage_NAME}.base ${Coverage_NAME}.total
 
         WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
