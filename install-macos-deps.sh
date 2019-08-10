822924437c81b44beb4ad7d0551a70ff101ec395
Luca Geretti <luca.geretti@gmail.com>
2019-08-10 16:00:49 +0200

    Removed GTK from installed dependencies on macOS

--- install-macos-deps.sh
+++ install-macos-deps.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-brew install lcov boost gtk cairo zlib
+brew install lcov boost cairo zlib
 wget https://sourceforge.net/projects/buddy/files/latest/download
 tar xvzf download
 cd buddy-2.4
