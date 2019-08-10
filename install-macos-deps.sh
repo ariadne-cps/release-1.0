7000497d06d35659eac7a120c55d26018586dba2
Luca Geretti <luca.geretti@gmail.com>
2019-08-10 16:35:56 +0200

    Fix export of pkg config path

--- install-macos-deps.sh
+++ install-macos-deps.sh
@@ -1,5 +1,4 @@
 #/bin/sh
-export PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig 
 brew install lcov boost cairo zlib
 wget https://sourceforge.net/projects/buddy/files/latest/download
 tar xvzf download
